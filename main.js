5ed4f8711e082b150087de76aeda0f4ee6fae409
Iqra Fatima <46266421+iqrafatimame@users.noreply.github.com>
2020-03-22 23:21:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+    if(!audio)  return
+    
+    key.classList.add("playing"); 
+    audio.currentTime = 0;
+    audio.play(); 
+   
+}
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove("playing");
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+console.log(keys);
+keys.forEach((key) => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',playSound);
